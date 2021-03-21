2cb4bebe13fdbb8b313c9c2def8244d41e515e5f
Mrthou <435680242@qq.com>
2021-03-21 21:14:05 +0800

    学会后台管理系统

---
+++ hpcf-manager/src/main/java/com/hainanu/hpcfmanager/HpcfManagerApplication.java
@@ -0,0 +1,13 @@
+package com.hainanu.hpcfmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpcfManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HpcfManagerApplication.class, args);
+    }
+
+}
---
+++ hpcf-manager/src/test/java/com/hainanu/hpcfmanager/HpcfManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hainanu.hpcfmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpcfManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
